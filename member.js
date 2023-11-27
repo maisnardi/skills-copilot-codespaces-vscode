1c641ea03bec87af6f669107e3798c769d1736c2
maisnardi <maisnardi@hotmail.com>
2023-11-27 12:04:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP'];
+    var member = {
+        name: 'Codegym',
+        age: 2,
+        address: 'Ha Noi',
+        skills: skills
+    };
+    return member.skills;
+}
